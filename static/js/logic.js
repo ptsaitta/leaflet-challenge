4ba5f226c0fb27d94664a44f4c8c928e64dee264
Patrick Saitta <ptsaitta@gmail.com>
2021-01-24 19:59:12 -0500

    fixed attribution, created map

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,8 +3,15 @@
 var mapInit = L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
-    id: "mapbox/dark-v10"
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    id: "mapbox/dark-v10",
     accessToken: API_KEY
   }
 );
+
+//create map, div id is "mapid"
+
+var map = L.map("mapid");
+
+
 
