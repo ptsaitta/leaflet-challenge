8b2bec6487ed66456a1bd9c5d10fd9229bbb60d5
Patrick Saitta <ptsaitta@gmail.com>
2021-01-24 20:08:02 -0500

    add map to init, added map options

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,13 +5,23 @@ var mapInit = L.tileLayer(
   {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     id: "mapbox/dark-v10",
+    tileSize: 512,
+    zoomOffset : -1,
     accessToken: API_KEY
   }
 );
 
 //create map, div id is "mapid"
 
-var map = L.map("mapid");
+var map = L.map("mapid", {
+    zoom: 3,
+    center: [40.7, -73.9]
+}
+);
+
+//..and add it to mapInit
+
+mapInit.addTo(map);
 
 
 
