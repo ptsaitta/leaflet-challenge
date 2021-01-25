d3bddeaa1c825ea31066a5212547fa0a18baa042
Patrick Saitta <ptsaitta@gmail.com>
2021-01-24 21:00:41 -0500

    assigned each earthquake a color and radius

--- static/js/logic.js
+++ static/js/logic.js
@@ -30,28 +30,47 @@ mapInit.addTo(map);
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
 
 //define functions to get color and radius, and set feature (earthquake) style to match those values
+//create function for style for each earthquake first, then use two more functions to get color and radius
+//magnitude at features -> properties -> mag
+//depth at third geometric coordinate
 
-//color is defined by feat DEPTH
+    function featStyle(feature) {
+        return{
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            radius: getRadius(feature.property.mag)
+        };
+    }
 
-function getColor(depth) = {
+
+
+
+    //color is defined by feat DEPTH
+
+    function getColor(depth) = {
     //switch will be useful to come up with 5 colors or so to bin earthquakes into
 
-    switch(true) {
-        case depth > 100:
-            return "#d41002";
-        case depth > 80:
-            return "#e04604";
-        case depth > 60:
-            return "#e08804";
-        case depth > 40:
-            return "#d9e004";
-        case depth > 20:
-            return "#8fe004";
-        default:
-            return "#07f22b";
+        switch(true) {
+            case depth > 100:
+                return "#d41002";
+            case depth > 80:
+                return "#e04604";
+            case depth > 60:
+                return "#e08804";
+            case depth > 40:
+                return "#d9e004";
+            case depth > 20:
+                return "#8fe004";
+            default:
+                return "#07f22b";
         }
     }
     
+    //radius is defined by feat MAGNITUDE
+
+    function getRadius(magnitude) {
+
+        return magnitude;
+    }
 
 
 
